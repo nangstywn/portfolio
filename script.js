788d35222f2d4887cc2fac4510a1e6a4bc36e21c
nangstywn <setyawannanang37@gmail.com>
2022-06-30 12:19:16 +0700

    update style

--- script.js
+++ script.js
@@ -9,18 +9,56 @@ $(document).ready(function(){
     $('.menu-btn').click(function(){
         $('.navbar ul').toggleClass('active')
         $('.menu-btn i').toggleClass('active')
+        $('ul .active').css('color','#f9004d')
     });
 
     $('li').click(function() {
-        // alert('a')
         $('.navbar ul').removeClass('active');
-        $('.menu-btn i').toggleClass('active')
-        // $(this).addClass('active');
-    // if($(this).find('a').attr('href') == '#tab-ccc') {
-    //     alert('ccc clicked');
-    // }
+        $('.menu-btn i').toggleClass('active');
+    });
+
+    $('.navbar ul li a').on('click', function(){
+        $('.navbar ul li a').removeClass('active');
+        $('.navbar ul li a').removeAttr('style');
+        $(this).addClass('active');
+    });
+    $(window).scroll(function(){
+    if ($('.navbar').hasClass("sticky")) {
+        $('.navbar ul li a').removeAttr('style');
+        $('.active').css('color','black')
+    }else{
+        $('.active').css('color','#f9004d')
+    }
+    })
 });
+$(document).ready(function() {
+		$('a[href*=#]').bind('click', function(e) {
+				e.preventDefault(); // prevent hard jump, the default behavior
+
+				var target = $(this).attr("href"); // Set the target as variable
+
+				// perform animated scrolling by getting top-position of target-element and set it as scroll target
+				$('html, body').stop().animate({
+						scrollTop: $(target).offset().top
+				}, 600, function() {
+						location.hash = target; //attach the hash (#jumptarget) to the pageurl
+				});
+				return false;
+		});
 });
+
+$(window).scroll(function() {
+		var scrollDistance = $(window).scrollTop();
+	
+		// Assign active class to nav links while scolling
+		$('.page-section').each(function(i) {
+				if ($(this).position().top <= scrollDistance) {
+						$('.navbar a.active').removeClass('active');
+						$('.navbar a').eq(i).addClass('active');
+				}
+		});
+}).scroll()
+
 var typed = new Typed('.typing', {
     strings: ['Backend Web Developer', 'Programmer'],
     typeSpeed: 100,
